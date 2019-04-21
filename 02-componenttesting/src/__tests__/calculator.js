21d5ed06e880814faaded02129b66d9eb7b84e5e
Arbey <arbpoe@hotmail.com>
2019-04-21 15:06:17 -0500

    Añadiendo tests para el componente Calculator

--- 02-componenttesting/src/__tests__/calculator.js
+++ 02-componenttesting/src/__tests__/calculator.js
@@ -1,9 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Calculator from '../calculator';
+import { shallow, configure } from 'enzyme';
+
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Calculator/>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
+it('renders TemperatureInput components', () => {
+  // arrange
+  const wrapper = shallow(<Calculator/>);
+
+  // act
+  const temperatureWrapper = wrapper.find('TemperatureInput');
+
+  // assert
+  expect(temperatureWrapper).toHaveLength(2);
+});
+
+it('renders BoilingVerdict components', () => {
+  // arrange
+  const wrapper = shallow(<Calculator/>);
+
+  // act
+  const boilingVerdictWrapper = wrapper.find('BoilingVerdict');
+
+  // assert
+  expect(boilingVerdictWrapper).toHaveLength(1);
+});
+
