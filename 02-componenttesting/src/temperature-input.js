779c3b196491c5c4b0bf3fb9567bb7eb2d16b7f6
Arbey <arbpoe@hotmail.com>
2019-04-20 23:22:08 -0500

    Empezando Component Testing

---
+++ 02-componenttesting/src/temperature-input.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default class TemperatureInput extends React.Component {
+  scaleNames = {
+    c: 'Celsius',
+    f: 'Fahrenheit'
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  handleChange = e => {
+    this.props.onTemperatureChange(e.target.value);
+  };
+
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {this.scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+      </fieldset>
+    );
+  }
+}
